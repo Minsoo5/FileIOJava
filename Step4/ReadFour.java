257eae6480e5b39f4ee6770e50d0b437d92df1ea
min <minsoojo5@gmail.com>
2023-07-07 12:55:07 -0400

    Step 4 completed

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,5 +1,6 @@
 package Step4;
 
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +13,41 @@ public class ReadFour
      */
         public static void main(String[] args)
         {
+
+            try {
+                System.out.println("Writing to file....");
+
+                PrintWriter fileOut = new PrintWriter("New text.txt");
+                fileOut.println("Hello ");
+                fileOut.close();
+
+                System.out.println("DONE!");
+
+
+                Scanner fileIn = new Scanner(new File("New text.txt"));
+                System.out.println("Reading the file....");
+
+
+                while (fileIn.hasNext()) {
+
+                    String textIn = fileIn.nextLine();
+                    String[] inputText = textIn.split(",");
+
+                    int sum = 0;
+                    for (int i = 0; i < inputText.length; i++) {
+                        sum += Integer.parseInt(inputText[i]);
+                    }
+
+                    System.out.println("Done!");
+                }
+
+            }
+
+
+
+            catch (IOException e) {
+                System.out.println("File not found");
+        }
             // Print out a running total of all the
             // values in the input file.
         }
